bf15f254f2b83177eedbdaab25ca866911623ac2
tanya_s <tanya_s@bk.ru>
2022-11-08 18:08:54 +0300

    update StepikSQL

--- Stepik_sql.sql
+++ Stepik_sql.sql
@@ -139,8 +139,135 @@ WHERE amount IN (
 SELECT author, title, price from book
 WHERE price < any (SELECT  min(price) from book group by author)
 
+
 --22 
+Создать таблицу поставка (supply), которая имеет ту же структуру, что и таблиц book.
+Поле 	Тип, описание
+supply_id 	INT PRIMARY KEY AUTO_INCREMENT
+title 	VARCHAR(50)
+author 	VARCHAR(30)
+price 	DECIMAL(8, 2)
+amount 	INT
+
+CREATE TABLE supply ( supply_id INT PRIMARY KEY AUTO_INCREMENT,
+ title VARCHAR(50), author VARCHAR(30), price DECIMAL(8, 2), amount INT);
+
+--23
+Занесите в таблицу supply четыре записи, чтобы получилась следующая таблица:
+supply_id 	title 	author 	price 	amount
+1 	Лирика 	Пастернак Б.Л. 	518.99 	2
+2 	Черный человек  	Есенин С.А. 	570.20 	6
+3 	Белая гвардия 	Булгаков М.А. 	540.50 	7
+4 	Идиот 	Достоевский Ф.М. 	360.80 	3
+
+INSERT INTO supply (title, author, price, amount) 
+VALUES 
+    ('Лирика','Пастернак Б.Л.', 518.99, 2),
+    ('Черный человек','Есенин С.А.', 570.20, 6),
+    ('Белая гвардия','Булгаков М.А.', 540.50, 7),
+    ('Идиот', 'Достоевский Ф.М.', 360.80, 3);
+
+--24
+Добавить из таблицы supply в таблицу book, все книги, кроме книг, написанных Булгаковым М.А. и Достоевским Ф.М.
+
+INSERT INTO book (title, author, price, amount) 
+SELECT title, author, price, amount 
+FROM supply
+WHERE author NOT IN ( "Булгаков М.А.", "Достоевский Ф.М." );
 
 
+--25
+Занести из таблицы supply в таблицу book только те книги, авторов которых нет в  book.
+
+INSERT INTO book (title, author, price, amount) 
+SELECT title, author, price, amount 
+FROM supply
+WHERE author NOT IN (SELECT author FROM book);
+
+
+--26
+Уменьшить на 10% цену тех книг в таблице book, количество которых принадлежит интервалу от 5 до 10, включая границы.
+
+UPDATE book
+SET price = price * 0.9 WHERE amount BETWEEN 5 AND 10;
+
+
+--27
+В таблице book необходимо скорректировать значение для покупателя в столбце buy таким образом, чтобы оно не превышало количество экземпляров книг, указанных в столбце amount. А цену тех книг, которые покупатель не заказывал, снизить на 10%
+
+UPDATE book 
+SET buy = IF(buy > amount, amount, buy), price = IF(buy = 0 , price * 0.9, price);
+SELECT * FROM book;
+
+
+--28
+Для тех книг в таблице book , которые есть в таблице supply, не только увеличить их количество в таблице book ( увеличить их количество на значение столбца amountтаблицы supply), но и пересчитать их цену (для каждой книги найти сумму цен из таблиц book и supply и разделить на 2)
+
+UPDATE book, supply 
+SET book.amount = book.amount + supply.amount,
+book.price = (book.price + supply.price)/2
+WHERE book.title = supply.title AND book.author = supply.author;
+
+
+--29
+Удалить из таблицы supply книги тех авторов, общее количество экземпляров книг которых в таблице book превышает 10.
+
+DELETE FROM supply 
+WHERE author IN(SELECT author FROM book
+     GROUP BY author
+     HAVING SUM(amount) > 10);
+
+
+--30
+Создать таблицу заказ (ordering), куда включить авторов и названия тех книг, количество экземпляров которых в таблице book меньше среднего количества экземпляров книг в таблице book. В таблицу включить столбец   amount, в котором для всех книг указать одинаковое значение - среднее количество экземпляров книг в таблице book.
+
+CREATE TABLE ordering AS
+SELECT author, title, 
+   (SELECT ROUND(AVG(amount)) FROM book) AS amount
+FROM book WHERE amount < (SELECT AVG(amount) FROM book);
+
+
+--31
+Вывести из таблицы trip информацию о командировках тех сотрудников, фамилия которых заканчивается на букву «а», в отсортированном по убыванию даты последнего дня командировки виде. В результат включить столбцы name, city, per_diem, date_first, date_last.
+
+SELECT name, city, per_diem, date_first, date_last
+FROM trip WHERE name like '%а %'
+ORDER BY date_last desc;
+
+
+--32
+Вывести в алфавитном порядке фамилии и инициалы тех сотрудников, которые были в командировке в Москве.
+
+SELECT DISTINCT name
+FROM trip WHERE city = 'Москва'
+ORDER BY name;
+
+
+--33
+Для каждого города посчитать, сколько раз сотрудники в нем были.  Информацию вывести в отсортированном в алфавитном порядке по названию городов. Вычисляемый столбец назвать Количество. 
+
+SELECT city, count(city) AS 'Количество' FROM trip 
+GROUP BY city
+ORDER BY city
+
+
+--34
+Вывести два города, в которых чаще всего были в командировках сотрудники. Вычисляемый столбец назвать Количество.
+
+SELECT city, count(name) as 'Количество' FROM trip
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 2
+
+
+--35
+Вывести информацию о командировках во все города кроме Москвы и Санкт-Петербурга (фамилии и инициалы сотрудников, город ,  длительность командировки в днях, при этом первый и последний день относится к периоду командировки). Последний столбец назвать Длительность. Информацию вывести в упорядоченном по убыванию длительности поездки, а потом по убыванию названий городов (в обратном алфавитном порядке).
+
+SELECT name,city,(DATEDIFF(date_last, date_first)+1) AS Длительность
+FROM trip WHERE city NOT IN ('Москва', 'Санкт-Петербург')
+ORDER BY Длительность DESC, city DESC;
+
+
+--36
 
 
