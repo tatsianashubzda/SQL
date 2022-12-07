68cfef0b97aa4227f856cf41acf3339c1cc1711b
tanya_s <tanya_s@bk.ru>
2022-12-07 17:42:32 +0300

    upd stepik.sql

--- Stepik_sql.sql
+++ Stepik_sql.sql
@@ -688,3 +688,31 @@ SELECT buy_id, name_client, SUM(price * buy_book.amount) as Стоимость
 FROM buy INNER JOIN client using(client_id) INNER JOIN buy_book using(buy_id) INNER JOIN book using(book_id)
 GROUP BY buy_book.buy_id
 ORDER BY 1;
+
+
+--74
+Вывести номера заказов (buy_id) и названия этапов,  на которых они в данный момент находятся. 
+Если заказ доставлен –  информацию о нем не выводить. Информацию отсортировать по возрастанию buy_id.
+
+SELECT buy_id, name_step
+FROM buy_step JOIN step USING(step_id)
+WHERE date_step_beg IS NOT NULL and date_step_end IS NULL
+ORDER BY buy_id
+
+
+--75
+
+В таблице city для каждого города указано количество дней, за которые заказ может быть доставлен в этот город
+ (рассматривается только этап Транспортировка). Для тех заказов, которые прошли этап транспортировки, вывести
+ количество дней за которое заказ реально доставлен в город. А также, если заказ доставлен с опозданием, 
+указать количество дней задержки, в противном случае вывести 0. 
+В результат включить номер заказа (buy_id), а также вычисляемые столбцы Количество_дней и Опоздание. 
+Информацию вывести в отсортированном по номеру заказа виде.
+
+SELECT buy_id, DATEDIFF(date_step_end, date_step_beg) AS Количество_дней, 
+IF(DATEDIFF(date_step_end, date_step_beg)<=days_delivery, 0, DATEDIFF(date_step_end, date_step_beg) - days_delivery) AS Опоздание
+FROM buy_step JOIN buy USING(buy_id) JOIN client USING(client_id) JOIN city USING(city_id)              
+WHERE step_id=3 AND DATEDIFF(date_step_end, date_step_beg) IS NOT NULL
+ORDER BY 1
+
+
