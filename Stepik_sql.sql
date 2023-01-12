c12ca023f1d3a92059da6a679a76f334b44726f9
tanya_s <tanya_s@bk.ru>
2023-01-12 11:32:18 +0300

    upd Stepik_sql

--- Stepik_sql.sql
+++ Stepik_sql.sql
@@ -792,5 +792,13 @@ VALUES
 
 --82
 Количество тех книг на складе, которые были включены в заказ с номером 5, уменьшить на то количество, которое в заказе с номером 5  указано.
+UPDATE book, buy_book
+SET    book.amount = book.amount - buy_book.amount
+WHERE  buy_book.buy_id = 5 AND book.book_id = buy_book.book_id
+
+
+--83
+
+
 
 
