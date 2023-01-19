263738efbcbc805d5a8bd44741f0219b569be07c
tanya_s <tanya_s@bk.ru>
2023-01-20 00:54:58 +0300

    upd Stepik_sql

--- Stepik_sql.sql
+++ Stepik_sql.sql
@@ -798,6 +798,20 @@ WHERE  buy_book.buy_id = 5 AND book.book_id = buy_book.book_id
 
 
 --83
+Cоздать счет (таблицу buy_pay) на оплату заказа с номером 5, в который включить название книг, их автора, цену, количество заказанных книг и  стоимость. Последний столбец назвать Стоимость. Информацию в таблицу занести в отсортированном по названиям книг виде.
+
+CREATE TABLE buy_pay AS
+SELECT 
+    title, name_author, book.price,buy_book.amount, book.price * buy_book.amount AS 'Стоимость'
+FROM
+    buy_book  INNER JOIN book USING (book_id) INNER JOIN author USING (author_id)
+WHERE
+    buy_id = 5
+ORDER BY title
+
+
+--84
+
 
 
 
