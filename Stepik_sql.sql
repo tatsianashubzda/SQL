2a3f0cc81fa93ae5db5a1db87d55b8d685eb661f
tanya_s <tanya_s@bk.ru>
2023-03-08 00:03:59 +0300

    upd Stepik_sql

--- Stepik_sql.sql
+++ Stepik_sql.sql
@@ -932,3 +932,14 @@ WHERE attempt_id = 7
 --95
 Посчитать результаты тестирования. Результат попытки вычислить как количество правильных ответов, деленное на 3 (количество вопросов в каждой попытке) и умноженное на 100. Результат округлить до двух знаков после запятой. Вывести фамилию студента, название предмета, дату и результат. Последний столбец назвать Результат. Информацию отсортировать сначала по фамилии студента, потом по убыванию даты попытки.
 
+SELECT name_student, name_subject, date_attempt, ROUND(SUM(is_correct)*100/3,2) Результат
+FROM answer
+    JOIN testing USING(answer_id)
+    JOIN attempt USING(attempt_id)
+    JOIN subject USING(subject_id)
+    JOIN student USING(student_id)
+GROUP BY name_student, name_subject, date_attempt
+ORDER BY name_student,  date_attempt DESC
+
+
+--96
