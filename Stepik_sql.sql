68568a8bbf6baf4661be8a4d359ce661ea365f45
tanya_s <tanya_s@bk.ru>
2023-04-07 01:20:19 +0300

    upd Stepik_sql

--- Stepik_sql.sql
+++ Stepik_sql.sql
@@ -1133,16 +1133,27 @@ ORDER BY p.name_program,  SUM(es.result) DESC;
 Например, Баранов Павел по «Физике» набрал 41 балл, а  для образовательной программы «Прикладная механика» минимальный балл по этому предмету определен в 45 баллов. Следовательно, абитуриент на данную программу не может поступить.
 
 SELECT name_program, name_enrollee
-FROM enrollee
-     JOIN program_enrollee USING(enrollee_id)
-     JOIN program USING(program_id)
-     JOIN program_subject USING(program_id)
-     JOIN subject USING(subject_id)
-     JOIN enrollee_subject USING(subject_id)
+FROM enrollee  JOIN program_enrollee USING(enrollee_id)
+JOIN program USING(program_id)
+JOIN program_subject USING(program_id)
+JOIN subject USING(subject_id)
+JOIN enrollee_subject USING(subject_id)
 WHERE enrollee_subject.enrollee_id = enrollee.enrollee_id and result < min_result
 ORDER BY name_program, name_enrollee
 
 
 
 --111
+Создать вспомогательную таблицу applicant,  куда включить id образовательной программы,  id абитуриента, сумму баллов абитуриентов (столбец itog) в отсортированном сначала по id образовательной программы, а потом по убыванию суммы баллов виде (использовать запрос из предыдущего урока).
+
+CREATE TABLE applicant
+SELECT program_id, enrollee.enrollee_id, SUM(result) AS itog
+FROM enrollee JOIN program_enrollee USING(enrollee_id)
+JOIN program USING(program_id)
+JOIN program_subject USING(program_id)
+JOIN subject USING(subject_id)
+JOIN enrollee_subject USING(subject_id)
+WHERE enrollee_subject.enrollee_id = enrollee.enrollee_id
+GROUP BY program_id, enrollee_id
+ORDER BY program_id, itog DESC;
 
