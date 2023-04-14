6d0848f22fdcc44de00f4c99a889b42b0ce221d9
tanya_s <tanya_s@bk.ru>
2023-04-14 21:04:32 +0300

    upd Stepik_sql

--- Stepik_sql.sql
+++ Stepik_sql.sql
@@ -1155,3 +1155,20 @@ WHERE enrollee_subject.enrollee_id = enrollee.enrollee_id
 GROUP BY program_id, enrollee_id
 ORDER BY program_id, itog DESC;
 
+
+
+
+--112
+Из таблицы applicant, созданной на предыдущем шаге, удалить записи, если абитуриент на выбранную образовательную программу не набрал минимального балла хотя бы по одному предмету (использовать запрос из предыдущего урока).
+
+DELETE FROM applicant
+USING applicant JOIN (
+    SELECT program_enrollee.program_id, program_enrollee.enrollee_id 
+    FROM program JOIN program_subject  USING(program_id)
+    JOIN program_enrollee USING(program_id)
+    JOIN enrollee_subject ON 
+    enrollee_subject.enrollee_id = program_enrollee.enrollee_id AND
+    enrollee_subject.subject_id = program_subject.subject_id
+    WHERE result < min_result ) AS t
+ ON applicant.program_id = t.program_id AND
+    applicant.enrollee_id = t.enrollee_id
