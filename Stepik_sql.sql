9c335c558bd6f59c294b9e619b0dd3576297d871
tanya_s <tanya_s@bk.ru>
2023-04-30 22:38:40 +0300

    upd Stepik_sql

--- Stepik_sql.sql
+++ Stepik_sql.sql
@@ -1197,3 +1197,20 @@ DROP TABLE applicant
 
 
 --115
+Включить в таблицу applicant_order новый столбец str_id целого типа , расположить его перед первым.
+
+ALTER TABLE applicant_order ADD
+str_id int FIRST
+
+
+
+
+--116
+Занести в столбец str_id таблицы applicant_order нумерацию абитуриентов, которая начинается с 1 для каждой образовательной программы.
+
+SET @row_num := 1;
+SET @num_pr := 0;
+UPDATE applicant_order
+SET str_id = IF(program_id = @num_pr, @row_num := @row_num + 1, @row_num := 1 AND @num_pr := @num_pr + 1)
+
+
