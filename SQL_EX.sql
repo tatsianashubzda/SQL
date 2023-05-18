1553ff628563c57317e42b52a3cb78c0dec4b0b1
tanya_s <tanya_s@bk.ru>
2023-05-18 10:24:29 +0300

    upd SQL_EX

--- SQL_EX.sql
+++ SQL_EX.sql
@@ -43,3 +43,18 @@ select model, ram, screen from Laptop where price > 1000
 Select * from Printer where  color = 'y'
 
 
+
+5. Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12x или 24x CD и цену менее 600 дол.
+
+Select model, speed, hd from pc 
+where   (cd = '12x' or cd = '24x' )  and  price < 600
+
+
+
+6. Для каждого производителя, выпускающего ПК-блокноты c объёмом жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов. Вывод: производитель, скорость.
+
+select distinct Product.maker, Laptop.speed  from product inner join  laptop  on Product.model = Laptop.model  where  laptop.hd>=10
+
+
+
+
